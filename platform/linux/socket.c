aa5b962686dc2216078204cdb4124fc31e253f10
symysak <yoshiaki.suyama@suyama.ne.jp>
2023-09-14 17:14:11 +0900

    Fix bug

--- platform/linux/socket.c
+++ platform/linux/socket.c
@@ -15,7 +15,7 @@ extern int sock_open(int *fd, int domain, struct sockaddr_storage *addr, socklen
         return -1;
     }
 
-    if(bind(*fd, addr, addr_len) == -1){
+    if(bind(*fd, (struct sockaddr *)addr, addr_len) == -1){
         fprintf(stderr, "Failed to bind socket: %s\n", strerror(errno));
         close(*fd);
         return -1;
@@ -30,7 +30,7 @@ extern int sock_close(int fd){
 
 extern ssize_t sock_read(int fd, uint8_t *buffer, size_t size, struct sockaddr_storage *addr, socklen_t *addr_len){
     ssize_t len;
-    len = recvfrom(fd, buffer, size, 0, addr, addr_len);
+    len = recvfrom(fd, buffer, size, 0, (struct sockaddr *)addr, addr_len);
     if(len <= 0){
         fprintf(stderr, "sock_read: %s\n", strerror(errno));
         return -1;
@@ -40,7 +40,7 @@ extern ssize_t sock_read(int fd, uint8_t *buffer, size_t size, struct sockaddr_s
 
 extern ssize_t sock_write(int fd, const uint8_t *frame, size_t size, struct sockaddr_storage *addr, socklen_t addr_len){
     ssize_t len;
-    len = sendto(fd, frame, size, 0, addr, addr_len);
+    len = sendto(fd, frame, size, 0, (struct sockaddr *)addr, addr_len);
     if(len <= 0){
         fprintf(stderr, "sock_write: %s\n", strerror(errno));
         return -1;
